c3da8ff877d6cdb3b74d8b55940c130d6d108efb
LAL-Better <52655359+LAL-Better@users.noreply.github.com>
2024-03-22 18:24:12 +0800

    Add ros2 demo
    More information please refer: https://blog.csdn.net/weixin_43328157/article/details/136915941

---
+++ 04-smart_car/ros2-demo/ros2_01/src/smart_car/src/publish_car.cpp
@@ -0,0 +1,47 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "smart_car/msg/car.hpp" //包含信息列表的头文件, 规则举例Car.msg->car.h；CarAbc.msg->car_abc.h
+
+using namespace std::chrono_literals;
+//以下创建一个节点及一个 smart_car TOPIC的发布者
+class CarPublisher : public rclcpp::Node
+{
+public:
+  CarPublisher()
+  : Node("smart_car_publisher")
+  {
+    smart_car_publisher_ =
+      this->create_publisher<smart_car::msg::Car>("smart_car", 100);
+
+//以下创建一个定时回调函数
+    auto publish_msg = [this]() -> void {
+        auto message = smart_car::msg::Car();
+
+        message.direction = message.D_UP;
+        message.note = "this is for test! ";
+
+        RCLCPP_INFO(this->get_logger(), "Direction: '%d' note: '%s'", message.direction, message.note.c_str());
+
+        this->smart_car_publisher_->publish(message);
+      };
+    //创建一个定时器，使每秒调用一次publish_msg函数
+    timer_ = this->create_wall_timer(1s, publish_msg);
+  }
+
+private:
+  //变量声明
+  rclcpp::Publisher<smart_car::msg::Car>::SharedPtr smart_car_publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CarPublisher>());
+  rclcpp::shutdown();
+
+  return 0;
+}
